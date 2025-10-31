8c743c94dc07c5256b71c159febb1b626dee0a54
quitquestdev <quitquestdev@gmail.com>
2025-10-31 14:14:34 -0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import QuitQuestRPG from '@/components/QuitQuestRPG'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <QuitQuestRPG />
+    </main>
+  )
+}
